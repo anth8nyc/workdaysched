39c0c0c1bb440d62af486b3f3943f41fbe0a4d03
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-25 22:32:47 -0500

    fixed local storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,77 +1,105 @@
-    $('.saveBtn').on('click', function() {
-        let inputText = $(this).siblings('.decription').val() ;
-        let time = $(this).parent().attr('id');
 
-        localStorage.setItem(time, inputText);
-        console.log(time);
-        console.log(inputText);
-        console.log($(this));
-        // $('notify').addClass('show');
-        // setTimeout(function(){
-        //     $('notify').removeClass('show');
-        // }, 5000); 
-    });
+let todaysDate = moment().format("dddd, MMMM Do YYYY");
+let currentHour = moment().hours();
+console.log(currentHour, " currentHour variable");
 
-    let todaysDate = moment().format("dddd, MMMM Do YYYY");
-    let currentHour = moment().hours();
-    console.log(currentHour, " currentHour variable");
+let saveBtnEl = document.querySelector(".saveBtn")
 
-    let timeInputs = [
+let timeInputs = [
         
-        { time: 09, input: "" },
-        { time: 10, input: "" },
-        { time: 11, input: "" },
-        { time: 12, input: "" },
-        { time: 13, input: "" },
-        { time: 14, input: "" },
-        { time: 15, input: "" },
-        { time: 16, input: "" },
-        { time: 17, input: "" },
+    { time: 09, input: "" },
+    { time: 10, input: "" },
+    { time: 11, input: "" },
+    { time: 12, input: "" },
+    { time: 13, input: "" },
+    { time: 14, input: "" },
+    { time: 15, input: "" },
+    { time: 16, input: "" },
+    { time: 17, input: "" },
         
-    ]
+]
 
-    function blockBuilder() {
-        for (let i = 0; i < timeInputs.length; i++) {
+function jumboTime(){
+    $('#currentDay').text(todaysDate);
+}
+
+function blockBuilder() {
+        
+    for (let i = 0; i < timeInputs.length; i++) {
             
-            let timeInp = timeInputs[i].time
-            console.log(timeInp + "time blocks")
-            let timeBlockEl = $(`<div class="row time-block " id="${timeInp}">
-                <div class="hour col-md-1">${timeInp}</div>
-                <textarea class="col-md-9 description"></textarea>
-                <button class="col-md-1 btn saveBtn" type="button">Save</button>
-                </div>`)
+        let timeInp = timeInputs[i].time;
+
+        if (timeInp > 12) {convTime = (timeInp - 12); 
+            antePostTime = convTime.toString() + ":00pm";}
+
+        else if (timeInp === 12) { antePostTime = timeInp.toString() + ":00pm";}
+        else if (timeInp === 0) { convTime =(time + 12);
+            antePostTime = convTime.toString() + ":00am";}
+
+        else {antePostTime = timeInp.toString() + ":00am";}
+
+        let textDisp = "";
+        storedInput = localStorage.getItem([timeInputs[i].time]);
+        
+        if (storedInput !== null) {
+
+           textDisp = timeInputs[i].input.concat(storedInput)
+
+        }; 
+
+        let timeBlockEl = $(`<div class="row time-block " id="${timeInp}">
+            <div class="hour col-md-1">${antePostTime}</div>
+            <textarea class="col-md-10 description">${textDisp}</textarea>
+            <button class="col-md-1 btn saveBtn"><i class="far fa-save"></i></button>
+            </div>`);
             
-            $(".container").append(timeBlockEl);
+        $(".container").append(timeBlockEl);
    
-        }
-        
-        timeUpdate();
+        console.log(timeInp);
+        console.log(antePostTime);
+    };
 
-    }
+    timeUpdate();
+}
 
-    function timeUpdate(){
-        console.log("Hello!")
-        $('.description').each(function (){
+function timeUpdate(){
+    
+    $('.description').each(function (){
                 
-            let blockH = parseInt($(this).parent().attr('id'));
-            console.log($(this))
-            console.log(blockH)
-            console.log(currentHour)
-            if (blockH < currentHour) {
-                $(this).addClass('past'); 
-            } else if (blockH > currentHour) {    
-                $(this).addClass('future'); 
-            } else {
-                $(this).addClass('present');                     
-            }      
-        })
+        let blockH = parseInt($(this).parent().attr('id'));
         
-    }
+        if (blockH < currentHour) {
+            $(this).addClass('past'); 
+        } else if (blockH > currentHour) {    
+            $(this).addClass('future'); 
+        } else {
+            $(this).addClass('present');                     
+        }      
+    })        
+}
 
-    function jumboTime(){
-        $('#currentDay').text(todaysDate);
-    }
+jumboTime();
+blockBuilder();
 
+$('.saveBtn').on('click', function() {
 
-    jumboTime();
-    blockBuilder();
+    let inputText = $(this).siblings('.description').val();
+    let cTime = $(this).parent().attr('id');
+        
+    localStorage.setItem(cTime, inputText);
+    console.log(cTime);
+    console.log(inputText);    
+    console.log($(this));
+
+    let successEl = $(`<div class="container"><div class="alert alert-success col-4 align-self-center role="alert">
+        Event saved! <i class="fas fa-check"></i>
+        </div></div>`)
+
+    $(".tb").prepend(successEl);
+    setTimeout(function(){
+        $(`.alert`).remove();
+    }, 2000); 
+            
+});
+
+ 
